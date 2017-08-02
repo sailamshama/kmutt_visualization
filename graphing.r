bd7c84fbbcade1cc69585033e14d89d5ab778028
sailamshama <saila.sms96@gmail.com>
2017-08-02 20:43:36 +0700

    loops stops at warning. need fix

--- graphing.r
+++ graphing.r
@@ -1,22 +1,38 @@
 library(ggplot2)
 
-i  = 0
-directory <- paste("/Users/saila/Desktop/Visualization_material/lean_data/",i , sep="")
-setwd(directory)
+names <- read.csv("/Users/saila/Desktop/Visualization_material/NewPathwayAPI_withName.txt", sep="\t")
+names = names[, 1:2]
+  
+for (i in 3:10){
+  directory <- paste("/Users/saila/Desktop/Visualization_material/lean_data/",i , sep="")
+  filename <- paste("/Users/saila/Desktop/Visualization_material/lean_data/",i,"/",i,"_sig-non.txt", sep="")
+  if (file.exists(filename)){
+    geneset_name = as.character(names[which(names[,1] == i), 2])
+    myGraph <- read.table(filename, header = T)	##Set path
+    suppressWarnings(myGraph$Weight <- as.numeric(as.character(myGraph$Weight)))
+    
+    png(paste(directory, "/profile",i,".png", sep=""))
+    ggplot(myGraph, aes(x=No, y=Weight))+
+      #scale_x_continuous(limits=c(0,600))
+      geom_point(aes(colour = Significant), show.legend=TRUE)+
+      xlab("gene-pair rank by weight")+
+      ylab("weight")+
+      ggtitle(paste(geneset_name, ": Weights",sep=""))+
+      theme(plot.title = element_text(hjust = 0.5))
+    dev.off()
+                
+    png(paste(directory, "/sig_pairs",i,".png", sep="")) 
+    ggplot(myGraph, aes(x=Significant, y=Weight))+
+      geom_point(aes(colour=Significant), show.legend = TRUE)+
+      xlab("")+
+      ylab("weight")+
+      ggtitle(paste(geneset_name, ": Dissimilarity between gene-pair",sep=""))+
+      theme(plot.title = element_text(hjust = 0.5))
+    dev.off()
+  }
+}
 
-filename <- paste("/Users/saila/Desktop/Visualization_material/lean_data/",i,"/",i,"_sig-non.txt", sep="")
-myGraph <- read.table(filename, header = T)	##Set path
-myGraph$Weight <- as.numeric(as.character(myGraph$Weight))
 
-png(paste(directory, "/profile",i,".png", sep=""))
-ggplot(myGraph, aes(x=No, y=Weight))+
-  geom_point(aes(colour = Significant), show.legend=TRUE)
-dev.off()
-            
-png(paste(directory, "/sig_pairs",i,".png", sep="")) 
-ggplot(myGraph, aes(x=Significant, y=Weight))+
-  trio_graph+geom_point(aes(colour=Significant), show.legend = TRUE)
-dev.off()
 
 #========================================================================
 #Noon's graphing
